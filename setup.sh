5f9ebcd4a5b399bfa17b268468ed570f477dae4d
Unzzui <98267393+Unzzui@users.noreply.github.com>
2022-01-25 17:20:03 -0300

    first

---
+++ setup.sh
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+=======
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+>>>>>>> f6b9c5f (initial)
+" > ~/.streamlit/config.toml
