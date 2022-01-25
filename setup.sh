23787d6098a9cb4fd12080f2c95e36773c80d440
Unzzui <98267393+Unzzui@users.noreply.github.com>
2022-01-25 17:35:07 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 mkdir -p ~/.streamlit/
 
 echo "\
@@ -11,18 +10,3 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-=======
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
->>>>>>> f6b9c5f (initial)
-" > ~/.streamlit/config.toml
