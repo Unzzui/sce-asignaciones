a120f3f087b2a6c6c5eda5e0b2f69389d3e8d924
Unzzui <98267393+Unzzui@users.noreply.github.com>
2022-05-30 13:00:11 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"diegoandres.bravobecerra@outlook.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
+[theme]\n\
+base = 'light'\n\
+\n\
 " > ~/.streamlit/config.toml
