29ff719ab9532cb7cc3b02a49e0e14596bb28ab8
Unzzui <98267393+Unzzui@users.noreply.github.com>
2022-05-30 15:41:18 -0400

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,3 +1,25 @@
 import streamlit as st
+from PIL import Image
 
-st.markdown("Hello World")
+
+# ---- Start App
+
+img = Image.open('images/oca.jpg')
+img1= Image.open('images/oca1.png')
+
+st.set_page_config(page_title="Asignaciones SCE", page_icon=img, layout="wide")
+# ---- Web App Title ----
+
+st.markdown(("""
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""),unsafe_allow_html=True)
+
+st.image(img1 , width=250)
+st.markdown('''
+#  Asignaciones SCE 
+Esta es una app web creada para facilitar las asignaciones realizadas.
+---
+''')
